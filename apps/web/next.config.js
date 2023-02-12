e18ada297ad478a295b433812b8cffcd173ac13b
minimalKim <kimin3004@gmail.com>
2023-02-12 16:22:31 +0900

    chore(root): 프로젝트 기본 구조 세팅

---
+++ apps/web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  optimizeFonts: false,
+  experimental: { esmExternals: false },
+};
+
+module.exports = nextConfig;
