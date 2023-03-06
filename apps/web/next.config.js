dea3c9d7bbb3f36e6c89cf4143df1b12030f8bcb
minimalKim <kimin3004@gmail.com>
2023-03-07 00:38:09 +0900

    chore(web): app directory로 사용

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: false,
-  experimental: { esmExternals: false },
+  experimental: {
+    esmExternals: false,
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
