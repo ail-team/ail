b3a7c2ea63c86d4e9648ecc879c2f9f418df8031
minimalKim <kimin3004@gmail.com>
2023-04-03 22:37:43 +0900

    chore(web): tailwind.config.js color 추가

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     './common/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        's-gray': {
+          100: '#131619',
+          80: '#353A3E',
+          40: '#A3A9AF',
+          20: '#DEE1E5',
+          10: '#F2F4F8',
+        },
+        's-purple': {
+          80: '#442086',
+          60: '#8143F3',
+        },
+        's-blue': {
+          60: '#2C61F5',
+          40: '#5BAFF8',
+        },
+      },
+    },
   },
   plugins: [],
 };
