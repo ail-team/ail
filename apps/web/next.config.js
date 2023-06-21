63284e82576c46d39f14bba917c0469689cfd849
minimalKim <kimin3004@gmail.com>
2023-06-21 22:17:08 +0900

    chore(web): env API_KEY 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 module.exports = nextConfig;
