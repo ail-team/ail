2fe0d6a64f6a96cc408424aa29075cf3589ac2db
minimalKim <kimin3004@gmail.com>
2023-06-21 22:46:56 +0900

    feat(web): color 상수 추가

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,29 +1,42 @@
+import { color } from './styles/color';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
-    './common/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        's-gray': {
-          100: '#131619',
-          80: '#353A3E',
-          40: '#A3A9AF',
-          20: '#DEE1E5',
-          10: '#F2F4F8',
-        },
-        's-purple': {
-          80: '#442086',
-          60: '#8143F3',
-        },
-        's-blue': {
-          60: '#2C61F5',
-          40: '#5BAFF8',
-        },
+export const content = ['./app/**/*.{js,ts,jsx,tsx}', './common/**/*.{js,ts,jsx,tsx}'];
+export const theme = {
+  extend: {
+    colors: {
+      'a-gray': {
+        100: color.gray[100],
+        95: color.gray[95],
+        90: color.gray[90],
+        85: color.gray[85],
+        80: color.gray[80],
+        60: color.gray[60],
+        40: color.gray[40],
+        20: color.gray[20],
+        10: color.gray[10],
+      },
+      'a-purple': {
+        80: color.purple[80],
+        60: color.purple[60],
+      },
+      'a-blue': {
+        60: color.blue[60],
+        40: color.blue[40],
+      },
+      'a-bp': {
+        100: color.blueToPurple[100],
+        90: color.blueToPurple[90],
+        80: color.blueToPurple[80],
+        70: color.blueToPurple[70],
+        60: color.blueToPurple[60],
+        50: color.blueToPurple[50],
+        40: color.blueToPurple[40],
+        30: color.blueToPurple[30],
+        20: color.blueToPurple[20],
+        10: color.blueToPurple[10],
       },
     },
   },
-  plugins: [],
 };
+export const plugins = [];
